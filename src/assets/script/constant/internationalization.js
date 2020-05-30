6ce06c82b7656fb38f01eef99468c69ef4b7e079
Zeongit <245142637@qq.com>
2020-05-30 10:50:46 +0800

    .

---
+++ src/assets/script/constant/internationalization.js
@@ -0,0 +1,2 @@
+export const FOLLOW_SIGN_IN_TITLE = "想要关注这个画师？"
+export const FOLLOW_SIGN_IN_TEXT = "请先登录，然后才能成为该画师的粉丝。"
---
+++ src/views/user/components/ShareBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ on: onMenu }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on: onTooltip }">
+          <v-btn
+            fab
+            text
+            class="mx-2"
+            :x-small="$vuetify.breakpoint.xsOnly"
+            v-on="Object.assign(onTooltip, onMenu)"
+          >
+            <v-icon>mdi-share-outline</v-icon>
+          </v-btn>
+        </template>
+        <span>分享</span>
+      </v-tooltip>
+    </template>
+    <v-list dense>
+      <v-list-item>
+        <v-list-item-content>
+          QQ空间
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          新浪微博
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>

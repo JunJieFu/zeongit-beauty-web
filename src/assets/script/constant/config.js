6e837bcf058fa9efb11949fac5ff81f29150d20d
Zeongit <245142637@qq.com>
2020-06-17 15:18:07 +0800

    .

--- src/assets/script/constant/config.js
+++ src/assets/script/constant/config.js
@@ -1,13 +1,17 @@
 export default {
   domain:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:12085"
+      ? "http://localhost:12081"
       : "http://www.secdra.com",
   host: process.env.NODE_ENV === "development" ? "/api" : "/api",
   accountHost:
     process.env.NODE_ENV === "development"
       ? "http://localhost:12180"
       : "http://account.secdra.com",
+  beautyHost:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:12181"
+      : "http://beauty.secdra.com",
   qiniuImg: "http://secdraimg.secdra.com",
   qiniuHead: "http://secdrahead.secdra.com",
   qiniuBack: "http://secdraback.secdra.com",

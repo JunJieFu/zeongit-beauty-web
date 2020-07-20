a6f4d806f1a4f5a65bea80072c789c44a00cfdc3
245142637@qq.com <asd+-*/00>
2020-07-20 17:35:09 +0800

    路由改用props

--- src/views/picture/Collection.vue
+++ src/views/picture/Collection.vue
@@ -50,12 +50,21 @@ export default {
   async created() {
     this.init()
   },
+  props: {
+    id: {
+      type: [String, null, undefined],
+      default: undefined
+    },
+    page: {
+      type: [String, Number],
+      default: 1
+    }
+  },
   data() {
     return {
       loading: false,
       pageable: new Pageable(0, 16, "lastModifiedDate,desc"),
-      currPage: null,
-      pictureId: null
+      currPage: null
     }
   },
   computed: {
@@ -73,7 +82,7 @@ export default {
     async init() {
       window.scrollTo(0, 0)
       this.MUpdateProgress(true)
-      await this.paging(this.$route.params.page, this.$route.params.id)
+      await this.paging(this.page, this.id)
       this.MUpdateProgress(false)
     },
     changePage(page) {
@@ -81,23 +90,20 @@ export default {
         return
       }
       if (this.mode === this.$enum.ListMode.WATERFALL.key) {
-        this.paging(page, this.pictureId)
+        this.paging(page, this.id)
       } else {
         this.$router.push(
-          `/picture/${encodeURIComponent(this.pictureId)}/collection/${page}`
+          `/picture/${encodeURIComponent(this.id)}/collection/${page}`
         )
       }
     },
-    async paging(pageIndex, pictureId = this.pictureId) {
+    async paging(pageIndex, pictureId) {
       if (
         !pictureId ||
         this.loading ||
         (this.currPage?.last && this.currPage.number <= pageIndex - 1)
       ) {
         return
-      }
-      if (pictureId !== this.pictureId) {
-        window.scrollTo(0, 0)
       }
       this.pageable.page = parseInt(pageIndex || 1) || 1
       this.loading = true
@@ -107,9 +113,6 @@ export default {
       )
       this.loading = false
       await this.$resultNotify(result)
-      if (pictureId !== this.pictureId) {
-        this.pictureId = pictureId
-      }
       this.currPage = result.data
     },
     follow({ detail, focus }) {

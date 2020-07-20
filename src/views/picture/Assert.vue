1b389659fa96120552283db2d5fae93a9c23d95b
Zeongit <245142637@qq.com>
2020-07-20 18:00:44 +0800

    分页排序默认createDate,desc

---
+++ src/views/picture/Assert.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row class="ma-0" justify="center" align="center" v-if="signInShow">
+    <v-col cols="12" sm="7" md="5" lg="4" xl="3"> </v-col>
+  </v-row>
+  <router-view v-else />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
